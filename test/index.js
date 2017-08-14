f888a830405433cccf72b316bff6ac004dd2f275
Phelipe Alves de Souza <sougyn@gmail.com>
2017-08-13 22:32:06 -0300

    - Updating test nfceGO

--- test/index.js
+++ test/index.js
@@ -15,99 +15,99 @@ describe('#NFCe-parser', function() {
 });
 
 describe('#NFCe-GO', function() {
-	var tmpNfce = null;
+	var nfceObj;
 	before(function(done){
 		nfceGO('52170826969401000148650040000140431040188685').then(nfce => {
-			tmpNfce = nfce;
+			nfceObj = nfce;
 			done();
 		});
 	});
 	it('NFCe-modelo', function(done) {
-		expect(tmpNfce.nfce.modelo).to.equal('65');
+		expect(nfceObj.nfce.modelo).to.equal('65');
 		done();
 	});
 	it('NFCe-serie', function(done) {
-		expect(tmpNfce.nfce.serie).to.equal('4');
+		expect(nfceObj.nfce.serie).to.equal('4');
 		done();
 	});
 	it('NFCe-numero', function(done) {
-		expect(tmpNfce.nfce.numero).to.equal('14043');
+		expect(nfceObj.nfce.numero).to.equal('14043');
 		done();
 	});
 	it('NFCe-dataEmissao', function(done) {
-		expect(tmpNfce.nfce.dataEmissao).to.equal('09/08/2017 19:13:30-03:00');
+		expect(nfceObj.nfce.dataEmissao).to.equal('09/08/2017 19:13:30-03:00');
 		done();
 	});
 	it('NFCe-dataEntrada', function(done) {
-		expect(tmpNfce.nfce.dataEntrada).to.equal(null);
+		expect(nfceObj.nfce.dataEntrada).to.equal(null);
 		done();
 	});
 	it('NFCe-valor', function(done) {
-		expect(tmpNfce.nfce.valor).to.equal('38,75');
+		expect(nfceObj.nfce.valor).to.equal('38,75');
 		done();
 	});
 	it('NFCe-cnpj', function(done) {
-		expect(tmpNfce.nfce.cnpj).to.equal('26.969.401/0001-48');
+		expect(nfceObj.nfce.cnpj).to.equal('26.969.401/0001-48');
 		done();
 	});
 	it('NFCe-nome', function(done) {
-		expect(tmpNfce.nfce.nome).to.equal('Alto Cerrado Supermercado Ltda');
+		expect(nfceObj.nfce.nome).to.equal('Alto Cerrado Supermercado Ltda');
 		done();
 	});
 	it('NFCe-inscricaoEstadual', function(done) {
-		expect(tmpNfce.nfce.inscricaoEstadual).to.equal('106835750');
+		expect(nfceObj.nfce.inscricaoEstadual).to.equal('106835750');
 		done();
 	});
 	it('NFCe-uf', function(done) {
-		expect(tmpNfce.nfce.uf).to.equal('GO');
+		expect(nfceObj.nfce.uf).to.equal('GO');
 		done();
 	});
 	it('NFCe-processo', function(done) {
-		expect(tmpNfce.nfce.processo).to.equal('0 - com aplicativo do Contribuinte');
+		expect(nfceObj.nfce.processo).to.equal('0 - com aplicativo do Contribuinte');
 		done();
 	});
 	it('NFCe-processoVer', function(done) {
-		expect(tmpNfce.nfce.processoVer).to.equal('1.02.53');
+		expect(nfceObj.nfce.processoVer).to.equal('1.02.53');
 		done();
 	});
 	it('NFCe-tipoEmissao', function(done) {
-		expect(tmpNfce.nfce.tipoEmissao).to.equal('1 - Normal');
+		expect(nfceObj.nfce.tipoEmissao).to.equal('1 - Normal');
 		done();
 	});
 	it('NFCe-finalidade', function(done) {
-		expect(tmpNfce.nfce.finalidade).to.equal('1 - Normal');
+		expect(nfceObj.nfce.finalidade).to.equal('1 - Normal');
 		done();
 	});
 	it('NFCe-natureza', function(done) {
-		expect(tmpNfce.nfce.natureza).to.equal('Venda de Mercadorias');
+		expect(nfceObj.nfce.natureza).to.equal('Venda de Mercadorias');
 		done();
 	});
 	it('NFCe-tipoOperacao', function(done) {
-		expect(tmpNfce.nfce.tipoOperacao).to.equal('1-saída');
+		expect(nfceObj.nfce.tipoOperacao).to.equal('1-saída');
 		done();
 	});
 	it('NFCe-formaPagamento', function(done) {
-		expect(tmpNfce.nfce.formaPagamento).to.equal('0 - À vista');
+		expect(nfceObj.nfce.formaPagamento).to.equal('0 - À vista');
 		done();
 	});
 	it('NFCe-digest', function(done) {
-		expect(tmpNfce.nfce.digest).to.equal('petwehlnyonJYlKNpxKjXz/CoVw=');
+		expect(nfceObj.nfce.digest).to.equal('petwehlnyonJYlKNpxKjXz/CoVw=');
 		done();
 	});
 	it('NFCe-eventos', function(done) {
-		expect(tmpNfce.nfce.eventos).to.equal('Autorização de Uso (Cód.: 110100)');
+		expect(nfceObj.nfce.eventos).to.equal('Autorização de Uso (Cód.: 110100)');
 		done();
 	});
 	it('NFCe-protocolo', function(done) {
-		expect(tmpNfce.nfce.protocolo).to.equal('152170805503001');
+		expect(nfceObj.nfce.protocolo).to.equal('152170805503001');
 		done();
 	});
 	it('NFCe-dataAutorizacao', function(done) {
-		expect(tmpNfce.nfce.dataAutorizacao).to.equal('09/08/2017 19:12:46-03:00');
+		expect(nfceObj.nfce.dataAutorizacao).to.equal('09/08/2017 19:12:46-03:00');
 		done();
 	});
 	it('NFCe-dataInclusao', function(done) {
-		expect(tmpNfce.nfce.dataInclusao).to.equal(null);
+		expect(nfceObj.nfce.dataInclusao).to.equal(null);
 		done();
 	});
 });
